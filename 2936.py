807b6f895eec783c7427d44cdffc2bf10b78f391
Eliézer.M <94458792+DarkBounty@users.noreply.github.com>
2022-09-17 00:36:01 -0300

    2936
    Problema 2936

---
+++ 2936.py
@@ -0,0 +1,8 @@
+curupica = int(input())
+boitata = int(input())
+boto = int(input())
+mapinguari = int(input())
+lara = int(input())
+
+print((curupica*300)+ (boitata * 1500) + ( boto * 600) + (mapinguari * 1000) + (lara * 150) + 225)
+
